4b7d36e0dadbfed8e99effb644a96fd8ef426eea
Victor Pongnian <victor.pongnian@gmail.com>
2016-10-11 23:20:31 +0100

    add missing directive files

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {enableProdMode} from '@angular/core';
-import {AppComponent, environment} from './app/index.ts';
+import {AppComponent, environment} from './app/index';
 
 if (environment.production) {
     enableProdMode();
